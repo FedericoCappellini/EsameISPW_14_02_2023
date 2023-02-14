374f07473e84dfaa88ba00f73420bbdc901c4d61
FredHats99 <federico.cappellini99@gmail.com>
2023-02-14 10:27:50 +0100

    commit 1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Main {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = true;
+        boolean c = branch(a,b);
+        System.out.println(c);
+    }
+
+    private static boolean branch(boolean A, boolean B) {
+        if(A){
+           return A;
+        }
+        if(B){
+            return B;
+        }
+        return false;
+    }
+}
